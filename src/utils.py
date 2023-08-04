deef28d43e03e9e79ad019f166ff7cb177b4d70c
philhoonoh <vlfgns5@naver.com>
2023-08-05 01:22:13 +0900

    adding new scripts for new dataset

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import jsonlines
 import re
 import json
-
+import yaml
 
 def read_jsonlines(file_path):
     data = []
@@ -25,3 +25,16 @@ def read_json_file(file_path):
         data = json.load(file)
 
     return data
+
+def get_keys(file_path):
+    with open(file_path, 'r') as file:
+        try:
+            yaml_data = yaml.safe_load(file)
+            # yaml_data now contains the parsed YAML data as a Python object
+            # You can access the data using dictionary-like syntax
+            # For example, if your YAML file has a key named 'foo', you can access its value like this:
+            # foo_value = yaml_data['foo']
+        except yaml.YAMLError as e:
+            print("Error while parsing YAML file:", e)
+
+    return yaml_data
