eb7efd148226bf35c0f7a26623fb2979c67c44d4
philhoonoh <vlfgns5@naver.com>
2023-08-05 03:34:02 +0900

    Adding zs-cot

--- mwp-zs-api.sh
+++ mwp-zs-api.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-python one-shot-cot-api.py \
---api_path /userhomes/philhoon/kt-ai-challenge/api.json \
---input_file /userhomes/philhoon/kt-ai-challenge/data/openai-gpt-3.5-turbo-0301-2048-0.0-KOR-gsm8k-test.jsonl \
---output_path /userhomes/philhoon/kt-ai-challenge/result \
---output_file_name one-shot-cot-result-ko-gsm8k-test.jsonl
+ python mwp-zs-api.py \
+ --api_path /userhomes/philhoon/kt-ai-challenge/api.json \
+ --input_file /userhomes/philhoon/kt-ai-challenge/data/public_mwp_data_v2_preprocess.jsonl \
+ --output_path /userhomes/philhoon/kt-ai-challenge/result/zero-shot-public_mwp_data_v2_preprocess
 
