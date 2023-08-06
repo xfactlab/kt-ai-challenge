849440b6299c746290153eee890102af8af784d8
philhoonoh <vlfgns5@naver.com>
2023-08-06 17:31:38 +0900

    Adding mwp-os-cot

---
+++ mwp-os-cot-api.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python mwp-os-cot-api.py \
+--api_path /userhomes/philhoon/kt-ai-challenge/api.json \
+--input_file /userhomes/philhoon/kt-ai-challenge/data/public_mwp_data_v2_reasoning.jsonl \
+--output_path /userhomes/philhoon/kt-ai-challenge/result/one-shot-cot-public_mwp_data_v2_test.jsonl
+
