77bd3ed06162e997cfd366918b6f2cd06a190007
philhoonoh <vlfgns5@naver.com>
2023-08-09 06:26:52 +0900

    adding result file

--- peft3-no-math5-Nshot-cot-api.sh
+++ peft3-no-math5-Nshot-cot-api.sh
@@ -2,53 +2,53 @@
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method os-cot \
+--method zs \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_addsub-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-1shot-cot-kor-D_addsub-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-kor-D_addsub-test.json
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method three-cot \
+--method zs-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_addsub-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-3shot-cot-kor-D_addsub-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-cot-kor-D_addsub-test.json
 
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method os-cot \
+--method zs \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_multiarith-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-1shot-cot-kor-D_multiarith-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-kor-D_multiarith-test.json
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method three-cot \
+--method zs-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_multiarith-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-3shot-cot-kor-D_multiarith-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-cot-kor-D_multiarith-test.json
 
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method os-cot \
+--method zs \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_single_eq-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-1shot-cot-kor-D_single_eq-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-kor-D_single_eq-test.json
 
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method three-cot \
+--method zs-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_single_eq-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-3shot-cot-kor-D_single_eq-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-cot-kor-D_single_eq-test.json
 
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method os-cot \
+--method zs \
 --input_file /userhomes/philhoon/kt-ai-challenge/data/public_mwp_data_v2_reasoning-test.jsonl \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-1shot-public_mwp_data_v2_reasoning-test.jsonl
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-public_mwp_data_v2_reasoning-test.jsonl
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-zs.json \
---method three-cot \
+--method zs-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/data/public_mwp_data_v2_reasoning-test.jsonl \
---output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-3shot-cot-public_mwp_data_v2_reasoning-test.jsonl
+--output_path /userhomes/philhoon/kt-ai-challenge/result/no-peft-zs-cot-public_mwp_data_v2_reasoning-test.jsonl
 
