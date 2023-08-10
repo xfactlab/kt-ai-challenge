59c42fb1ac727dc3f46c4d34ca394d34b3dbeeee
philhoonoh <vlfgns5@naver.com>
2023-08-10 19:10:39 +0900

    adding files

--- peft3-math5-eval.sh
+++ peft3-math5-eval.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python peft3-math5-os-cot-api.py \
---api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
---method zs \
---input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_addsub-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-zs-kor-D_addsub-test.json
+#python peft3-math5-os-cot-api.py \
+#--api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
+#--method zs \
+#--input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_addsub-test.json \
+#--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-zs-kor-D_addsub-test.json
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
@@ -16,20 +16,20 @@ python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
 --method os-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_addsub-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft-4-1shot-cot-kor-D_addsub-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-1shot-cot-kor-D_addsub-test.json
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
 --method three-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_addsub-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft-4-3shot-cot-kor-D_addsub-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-3shot-cot-kor-D_addsub-test.json
 
 
-python peft3-math5-os-cot-api.py \
---api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
---method zs \
---input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_multiarith-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-zs-kor-D_multiarith-test.json
+#python peft3-math5-os-cot-api.py \
+#--api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
+#--method zs \
+#--input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_multiarith-test.json \
+#--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-zs-kor-D_multiarith-test.json
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
@@ -42,19 +42,19 @@ python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
 --method os-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_multiarith-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft-4-1shot-cot-kor-D_multiarith-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-1shot-cot-kor-D_multiarith-test.json
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
 --method three-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_multiarith-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft-4-3shot-cot-kor-D_multiarith-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-3shot-cot-kor-D_multiarith-test.json
 
-python peft3-math5-os-cot-api.py \
---api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
---method zs \
---input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_single_eq-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-zs-kor-D_single_eq-test.json
+#python peft3-math5-os-cot-api.py \
+#--api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
+#--method zs \
+#--input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_single_eq-test.json \
+#--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-zs-kor-D_single_eq-test.json
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
@@ -66,13 +66,13 @@ python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
 --method os-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_single_eq-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft-4-1shot-cot-kor-D_single_eq-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-1shot-cot-kor-D_single_eq-test.json
 
 
 python peft3-math5-os-cot-api.py \
 --api_path /userhomes/philhoon/kt-ai-challenge/api-peft4.json \
 --method three-cot \
 --input_file /userhomes/philhoon/kt-ai-challenge/eng_data/kor-D_single_eq-test.json \
---output_path /userhomes/philhoon/kt-ai-challenge/result/peft-4-3shot-cot-kor-D_single_eq-test.json
+--output_path /userhomes/philhoon/kt-ai-challenge/result/peft4-3shot-cot-kor-D_single_eq-test.json
 
 
